88deb64390c30e8fa246b056f0c6ee0fdb477b22
jason60810 <jason60810@gmail.com>
2023-01-03 12:48:12 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script lang="ts" setup>
+import HelloWorld from "@/components/HelloWorld.vue";
+import Default from "@/layouts/default/Default.vue";
+</script>
