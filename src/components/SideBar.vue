2fd1b26eac3ce0a32596804c554585707885862f
jason60810 <jason60810@gmail.com>
2023-01-05 13:06:33 +0800

    fix: sidebar active style

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -4,12 +4,14 @@
       <VListItem
         prepend-icon="mdi-folder"
         title="熱門"
-        @click="memeStore.getMemes"
+        @click="getMemes"
+        value="popular"
+        :active="isPopular"
       ></VListItem>
       <VListItem
         v-for="memeClass in memeStore.memeClasses"
         :key="memeClass.id"
-        @click="memeStore.getMemesByClassId(memeClass.id)"
+        @click="getMemesByClassId(memeClass.id)"
         prepend-icon="mdi-folder"
         :title="memeClass.name"
         :value="memeClass.id"
@@ -23,10 +25,21 @@ import { useMemeStore } from "@/store/meme";
 import { ref } from "vue";
 
 const drawer = ref(true);
+const isPopular = ref(true);
 const memeStore = useMemeStore();
 
 function toggleDrawer() {
   drawer.value = !drawer.value;
+}
+
+function getMemes() {
+  memeStore.getMemes();
+  isPopular.value = true;
+}
+
+function getMemesByClassId(memeClassId: number) {
+  memeStore.getMemesByClassId(memeClassId);
+  isPopular.value = false;
 }
 
 defineExpose({ toggleDrawer });
